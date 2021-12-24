b4995f4965f9836bfaa013b3e690f2235b903964
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2021-12-24 16:00:41 +0530

    kya

--- git_processor.py
+++ git_processor.py
@@ -1,13 +1,17 @@
 from requests import get
+from os import path
+from os import remove
 import json
 
 def fetch_git_repos(uname):
-    
     if uname:
         url = f"https://api.github.com/users/{uname}/repos?per_page=100"
         response = get(url)
         jres = json.loads(response.text)
-        with open(f"{uname}_repo.json","w") as f:
+        output_file = f"{uname}_repo.json"
+        if(path.exists(output_file)):
+            remove(output_file)
+        with open(output_file,"w") as f:
             f.writelines(json.dumps(jres))
     else:
         raise Exception("ERROR: Invalid username given")
